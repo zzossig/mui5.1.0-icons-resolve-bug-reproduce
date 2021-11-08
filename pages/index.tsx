76d2110a5d78cf6aae68dff12a2181544f9ce187
zzossig <zzossig@gmail.com>
2021-11-09 03:22:36 +0900

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import type { NextPage } from "next";
+import AccessAlarmIcon from "@mui/icons-material/AccessAlarm";
+import ThreeDRotation from "@mui/icons-material/ThreeDRotation";
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <AccessAlarmIcon />
+      <ThreeDRotation />
+    </div>
+  );
+};
+
+export default Home;
